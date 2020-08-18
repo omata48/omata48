4b50a558d6dc046b3e7dfc2dc4844119c4047285
Omar <62365988+omata48@users.noreply.github.com>
2020-08-18 15:43:20 -0500

    finished deployment stuff

--- server.js
+++ server.js
@@ -5,11 +5,18 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+if(process.end.NODE_ENV === "production"){
+    app.use(express.static("client/build"));
+}
 
 app.get("/api/test", function(req,res) {
     res.json({
         test: "test"
     })
+});
+
+app.get("*", function(req, res) {
+    res.sendFile("./client/build/index.html");
 })
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
