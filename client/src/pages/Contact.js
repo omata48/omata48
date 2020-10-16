e16fddb766111964640614f7de411ca7f6b6350f
Omar <62365988+omata48@users.noreply.github.com>
2020-10-15 23:48:28 -0500

    updates to contact page

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,17 +1,28 @@
 import React from 'react';
-import { SocialIcon } from 'react-social-icons';
+import GitIcon from '../utils/images/icons/github.png';
+import Email from '../utils/images/icons/email.png';
+import LinkedIn from '../utils/images/icons/linkedin.png'
 
 function Contact() {
     return (
-        <section id="bodyMain" className="container my-5 d-flex">
-            <div className="card border-light col-12">
+        <section id="Contact" className="d-flex">
+            <div className="card border-light col-12 py-4">
                 <div className="card-body">
                     <header className="mb-1"><h1>Get in touch:</h1></header>
                     <hr style={{ border: "2px solid grey" }} className="mb-4" />
-                    <ul className="list-group list-group-flush" style={{ alignItems: "baseline"}}>
-                        <li className="list-group-item align-left"><SocialIcon url='mailto:omata48@outlook.com?subject=Hello Omar (portfolio)' bgColor="#343A40" /> <span className="h4 ml-3">Send me an email: omata48@outlook.com</span></li>
-                        <li className="list-group-item align-left"><SocialIcon target="_blank" url='https://github.com/omata48' bgColor="#848484" /> <span className="h4 ml-3">My Projects on Github: https://github.com/omata48</span> </li>
-                        <li className="list-group-item align-left"><SocialIcon target="_blank" url='https://www.linkedin.com/in/omar-mata' bgColor="#343A40"/> <span className="h4 ml-3">Connect with me on LinkedIn: https://www.linkedin.com/in/omar-mata/</span> </li>
+                    <ul className="list-group list-group-flush" style={{ alignItems: "baseline",width: "100%"}}>
+                        <li className="list-group-item align-left">
+                            <a target="_blank" rel='noopener noreferrer' href='mailto:omata48@outlook.com?subject=Hello%20Omar%20(portfolio)'><img src={Email} alt='email icon' /></a>
+                            <span className="h4 ml-3">Send me an email: omata48@outlook.com</span>
+                        </li>
+                        <li className="list-group-item align-left">
+                            <a target="_blank" rel='noopener noreferrer' href='https://github.com/omata48'><img src={GitIcon} alt='github icon'/></a>
+                            <span className="h4 ml-3">My Projects on Github: https://github.com/omata48</span> 
+                        </li>
+                        <li className="list-group-item align-left">
+                            <a target="_blank" rel='noopener noreferrer' href='https://www.linkedin.com/in/omar-mata'><img src={LinkedIn} alt='linkedin icon' /></a> 
+                            <span className="h4 ml-3">Connect with me on LinkedIn: https://www.linkedin.com/in/omar-mata/</span> 
+                        </li>
                     </ul>
                 </div>
             </div>
