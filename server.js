1eebbd76c54f39c7ab779e6293dd86506eb72b96
Omar <62365988+omata48@users.noreply.github.com>
2020-10-15 23:48:37 -0500

    server static files updated

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("./client/build"))
 if(process.env.NODE_ENV === "production"){
     app.use(express.static("client/build"));
 }
@@ -15,8 +17,8 @@ if(process.env.NODE_ENV === "production"){
 //     })
 // });
 
-app.get("*", function(req, res) {
-    res.sendFile("./client/build/index.html");
+app.use(function(req, res) {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
 })
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
